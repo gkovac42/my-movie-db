43a22b8e7ac56edb68ac02952f86e284df595ae7
Goran <gkovac89@gmail.com>
2018-01-15 21:05:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/goran/mymoviedb/data/CreditsInteractor.java
@@ -0,0 +1,15 @@
+package com.example.goran.mymoviedb.data;
+
+import com.example.goran.mymoviedb.movies.details.MovieCreditsContract;
+
+/**
+ * Created by Goran on 15.1.2018..
+ */
+
+public class CreditsInteractor implements MovieCreditsContract.Model {
+
+    @Override
+    public void getCredits() {
+
+    }
+}
